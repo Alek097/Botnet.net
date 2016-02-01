503d841e6e17e6ef25791ce3bc61fe3dcab5f6a4
Alek97 <petrovskii097@gmail.com>
2016-02-01 17:38:26 +0300

    Теперь Botnet.net и BotLib переведены под .NETFramework 2.0

--- Botnet.net/Botnet.net/BotBrowser.cs
+++ Botnet.net/Botnet.net/BotBrowser.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Botnet.net
