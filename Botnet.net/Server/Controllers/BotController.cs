1375060783f6cb4fb33de05e30bea866e64402e2
Alek97 <petrovskii097@gmail.com>
2016-02-03 21:35:55 +0300

    Добавил действия контроллера Обновления и Регистрации в BotController

--- Botnet.net/Server/Controllers/BotController.cs
+++ Botnet.net/Server/Controllers/BotController.cs
@@ -13,9 +13,17 @@ namespace Server.Controllers
             Так же думаю что нужно использовать авторизацию бота, чтобы если кто-то пронюхал как отправить запрос, то не смог бы получить ответ с заданием.
         */
         // GET: Bot
-        public string GetTask(Guid id)
+        public string GetTask()
         {
             return "visit;http://www.twitch.tv/kanalkarnaval;0:1:0"; //Потому что нам не нужен только текст, а не html документ
+        }
+        public string Update(string version)
+        {
+            return string.Empty;//Возвращает пустую строку.
+        }
+        public string Registr(Guid id)
+        {
+            return string.Empty;//Возвращает пустую строку.
         }
     }
 }
