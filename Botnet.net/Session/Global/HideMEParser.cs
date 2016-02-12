76392b5bc849217d7f3578c8eb40c1f52717eac2
Alek097 <petrovskii097@gmail.com>
2016-02-12 14:00:43 +0300

    Добавлен класс парсер для HideME.ru. HideMEParser реализующий интерфейс IParser

---
+++ Botnet.net/Session/Global/HideMEParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session.Global
+{
+    class HideMEParser : IParser
+    {
+        public List<ProxyServer> GetProxyServerList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
