125637526af35723dc8d4fae0976ffe357be26e9
Alek097 <petrovskii097@gmail.com>
2016-02-12 20:00:07 +0300

    Добавил форму Page для получения разметки страницы, т.к. некоторые сайты защищены от парсинга

---
+++ Botnet.net/Session/Global/Page.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session.Global
+{
+    
+    public partial class Page : Form
+    {
+        private bool _pageIsLoad;
+        public Page(Uri url)
+        {
+            InitializeComponent();
+            this.Browser.DocumentCompleted += new
+                 WebBrowserDocumentCompletedEventHandler(
+                (sender, e) =>
+                this._pageIsLoad = true);
+            this.Browser.Url = url;
+        }
+        public string GetPage()
+        {
+            while (!_pageIsLoad) ;
+            return this.Browser.DocumentText;
+        }
+    }
+}
