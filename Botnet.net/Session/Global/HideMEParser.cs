cec7674dab5116da4b867bf6159e1d18f38f1a59
Alek097 <petrovskii097@gmail.com>
2016-02-12 20:10:23 +0300

    Добавил скачаивание страницы в HideMEParser

--- Botnet.net/Session/Global/HideMEParser.cs
+++ Botnet.net/Session/Global/HideMEParser.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
@@ -10,16 +10,32 @@ namespace Session.Global
 {
     class HideMEParser : IParser
     {
-        private List<ProxyServer> _proxys;
+        private List<ProxyServer> _proxys = new List<ProxyServer>();
 
         public HideMEParser()
         {
-            
+            UpdateList();
         }
 
         public List<ProxyServer> GetProxyServerList()
         {
-            throw new NotImplementedException();
+            return this._proxys;
+        }
+        private void UpdateList()
+        {
+            string HTML;
+            using (Page p = new Page(new Uri("http://hideme.ru/proxy-list/?maxtime=800&type=h")))
+            {
+                p.Show();
+                HTML = p.GetPage();
+                p.Close();
+            }
+        }
+        private void SetList(string HTML)
+        {
+            string pattern = @"<tbody>[.\s]+?</tbody>";
+
+            string tbody = Regex.Match(HTML, pattern, RegexOptions.IgnoreCase).ToString();
         }
     }
 }
