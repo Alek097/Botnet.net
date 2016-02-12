61c2dac4f75b34cd8621035e0b527c0143167359
Alek097 <petrovskii097@gmail.com>
2016-02-12 13:41:02 +0300

    Добавлена структура Proxy

---
+++ Botnet.net/Session/Global/Proxy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session.Global
+{
+    struct Proxy
+    {
+        public string IP { get; set; }
+        public string Port { get; set; }
+
+        public override string ToString()
+        {
+            return IP + ":" + Port;
+        }
+    }
+}
